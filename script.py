c2c7040dbbe739d7090a16ad894823608a0396be
Elin Söderberg <elin.j.soderberg@gmail.com>
2016-11-21 12:46:14 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,16 @@
+fej = "ur"
 
+original = raw_input('Enter a word: ')
+
+word = original.lower()
+
+if len(original) > 0 and original.isalpha() and word:
+    print word
+else:
+    print: 'empty'
+        
+first = word[0]
+
+new_word = word[1:] + first + fej
+    print: new_word
+        
